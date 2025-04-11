259a02ee306279b7504e337a212edc33b537c98e
dominik <01197296@pw.edu.pl>
2025-04-11 13:05:16 +0200

    Add UrlTranslate entity

---
+++ short-back/url-shortener/src/main/java/pl/yapyap/urlshortener/entity/UrlTranslate.java
@@ -0,0 +1,31 @@
+package pl.yapyap.urlshortener.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "url_translate")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class UrlTranslate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+
+    @Column(name = "short_url")
+    String shortUrl;
+
+    @Column(name = "long_url")
+    String longUrl;
+
+    @Column(name = "created_at")
+    LocalDateTime createdAt;
+}

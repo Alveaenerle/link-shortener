b888aa4b6e39c2cd32683f1eecc1b741902fac60
dominik <01197296@pw.edu.pl>
2025-04-11 13:07:45 +0200

    Add UrlTranslate repository

---
+++ short-back/url-shortener/src/main/java/pl/yapyap/urlshortener/repository/UrlTranslateRepository.java
@@ -0,0 +1,9 @@
+package pl.yapyap.urlshortener.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.yapyap.urlshortener.entity.UrlTranslate;
+
+@Repository
+public interface UrlTranslateRepository extends JpaRepository<Integer, UrlTranslate> {
+}

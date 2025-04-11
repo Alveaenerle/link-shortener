813ecc4f55fa2b3cc6eb4b707152c62bbfea0c30
dominik <01197296@pw.edu.pl>
2025-04-11 16:26:54 +0200

    Create ApiHandler for communication with backend api

---
+++ short-front/src/utils/ApiHandler.js
@@ -0,0 +1,36 @@
+const baseUrl = `${REACT_APP_API_URL}`
+
+const post_plain = async (url, body) => {
+    try {
+        const response = await fetch(`${baseUrl}${url}`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: body,
+        })
+
+        return response;
+    } catch {
+        // TODO redirect to error page
+        console.error('Error:', error);
+    }
+}
+
+const get_plain = async (url) => {
+    try {
+        const response = await fetch(`${baseUrl}${url}`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            }
+        })
+
+        return response;
+    } catch {
+        // TODO redirect to error page
+        console.error('Error:', error);
+    }
+}
+
+export {post_plain, get_plain};
